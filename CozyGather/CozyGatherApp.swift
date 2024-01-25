79cf41df59432b25f69cc7853b8c6143c5aca36c
Aayushi-4094 <aayushiagarwal580@gmail.com>
2024-01-25 20:55:08 +0530

    Initial Commit

---
+++ CozyGather/CozyGatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  CozyGatherApp.swift
+//  CozyGather
+//
+//  Created by user2 on 25/01/24.
+//
+
+import SwiftUI
+
+@main
+struct CozyGatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
