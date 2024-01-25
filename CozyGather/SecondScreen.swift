d6010e94491b8fb3e8abe0cfe018299bddce70e6
Aayushi-4094 <aayushiagarwal580@gmail.com>
2024-01-25 21:14:27 +0530

    Added the files

--- CozyGather/SecondScreen.swift
+++ CozyGather/SecondScreen.swift
@@ -1,8 +1,24 @@
-//
-//  SecondScreen.swift
-//  CozyGather
-//
-//  Created by user2 on 25/01/24.
-//
+// SecondView.swift
+import SwiftUI
 
-import Foundation
+struct SecondScreen: View {
+    var body: some View {
+        NavigationView {
+            NavigationLink(destination: ThirdScreen()) {
+                VStack(spacing: 3) {
+                    Image("logo1")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 1/2)
+
+                    Image("logo2")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 1/2)
+                        .padding(.bottom, 10)
+                }
+            }
+            .navigationBarHidden(true)
+        }
+    }
+}
--- CozyGather/ThirdScreen.swift
+++ CozyGather/ThirdScreen.swift
@@ -1,8 +1,59 @@
-//
-//  ThirdScreen.swift
-//  CozyGather
-//
-//  Created by user2 on 25/01/24.
-//
+// ThirdView.swift
+import SwiftUI
 
-import Foundation
+struct ThirdScreen: View {
+    var body: some View {
+        VStack {
+            Text("Choose your Profile")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.top, 20)
+
+            Spacer()
+
+            HStack(spacing: 0) {
+                VStack {
+                    Button(action: {
+                        // Add action for the Supplier button
+                        print("Supplier Button Tapped")
+                    }) {
+                        Image("supplier_logo") // Replace with the actual image name for the supplier
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: UIScreen.main.bounds.width / 3, height: UIScreen.main.bounds.height / 4)
+                            .cornerRadius(10)
+                    }
+
+                    Text("Supplier")
+                        .font(.subheadline)
+                        .padding(.top, 5)
+                }
+
+                Divider()
+
+                VStack {
+                    Button(action: {
+                        // Add action for the User button
+                        print("User Button Tapped")
+                    }) {
+                        Image("user_logo") // Replace with the actual image name for the user
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: UIScreen.main.bounds.width / 3, height: UIScreen.main.bounds.height / 4)
+                            .cornerRadius(10)
+                    }
+
+                    Text("User")
+                        .font(.caption)
+                        .padding(.top, 5)
+                }
+            }
+            .padding(.horizontal, 20)
+            .padding(.vertical, 20)
+
+            Spacer()
+        }
+        .navigationBarHidden(true)
+    }
+}
+
