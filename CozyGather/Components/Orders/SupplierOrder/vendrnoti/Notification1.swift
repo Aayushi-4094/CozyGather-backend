a93e9a8a3d0f449313a28846906b18a041628019
Srivatsal Narayan <narayansrivatsal@gmail.com>
2024-02-03 16:21:44 +0530

    edited vendr noti

--- CozyGather/Components/Orders/SupplierOrder/vendrnoti/Notification1.swift
+++ CozyGather/Components/Orders/SupplierOrder/vendrnoti/Notification1.swift
@@ -2,65 +2,99 @@ import SwiftUI
 
 struct Notification1: View {
     var body: some View {
-        VStack(spacing: 20) {
-            HStack {
-                Button(action: {
-                    // Add action for the back button
-                }) {
-                    Image(systemName: "chevron.left")
-                        .font(.title)
-                        .foregroundColor(.blue)
-                        .position(CGPoint(x: 40.0, y: -200.0))
+        VStack {
+            VStack{
+                HStack {
+                    Button(action: {
+                        // Add action for the back button
+                    }) {
+                        Image(systemName: "chevron.left")
+                            .font(.title)
+                            .foregroundColor(.blue)
+                    }
+                    Spacer()
+                    
+                    Label("Notification", systemImage: "")
+                        .font(Font.custom("AirbnbCereal_W_Md", size: 24))
+                        .foregroundColor(Color(red: 0.07, green: 0.05, blue: 0.15))
+                    
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        // Add action for the bell button
+                    }) {
+                        Image(systemName: "bell")
+                            .font(.title)
+                            .foregroundColor(.blue)
+                        
+                    }
                 }
-                Spacer()
-
-                Label("Notification", systemImage: "")
-                    .font(Font.custom("AirbnbCereal_W_Md", size: 24))
-                    .foregroundColor(Color(red: 0.07, green: 0.05, blue: 0.15))
-                    .padding(.bottom, 20)
-                    .position(CGPoint(x: 0.0, y: -190.0))
-
-
-                Spacer()
-
-                Button(action: {
-                    // Add action for the bell button
-                }) {
-                    Image(systemName: "bell")
-                        .font(.title)
-                        .foregroundColor(.blue)
-                        .position(CGPoint(x: 100.0, y: -200.0))
-
-                }
+                .padding()
+                .background(Color.white)
+                Divider()
+                    .background(Color.black)
+                    .position(CGPoint(x: 180, y: 0))
             }
-            .padding()
-            .background(Color.white)
-            .padding(.top , -120)
-            .frame(width: 450, height: 10)// Push everything down by adding top padding
+            
+            VStack{
+                
+                CustomNotificationBox1(imageName: "noti1", date: "January 25, 2024", description: "Hi.",time: "just now")
+                    .padding()
+                    .position(CGPoint(x: 210.0, y: -600.0))
+                    .frame(width: 430, height: 10)
+                
+                
+                CustomNotificationBox1(imageName: "noti2", date: "February 2, 2024", description: "Hello.",time: "JustNow")
+                    .padding()
+                    .position(CGPoint(x: 210.0, y: -500.0))
+                    .frame(width: 430, height: 10)
+                
+            }
+        }
+         
+    }
+}
 
-            CustomNotificationBox1(imageName: "noti1", date: "January 25, 2024", description: "Hi.", timeButtonText: "")
-                .padding()
-                .position(CGPoint(x: 210.0, y: -250.0))
-                .frame(width: 430, height: 10)
 
-            CustomNotificationBox1(imageName: "noti2", date: "February 2, 2024", description: "Hello.", timeButtonText: "")
+import SwiftUI
+
+struct CustomNotificationBox1: View {
+    var imageName: String
+    var date: String
+    var description: String
+    var time:String
+    
+    var body: some View {
+        HStack {
+            Image(imageName)
+                .resizable()
+                .frame(width: 100, height: 100)
                 .padding()
-                .position(CGPoint(x: 210.0, y: -150.0))
-                .frame(width: 430, height: 10)
+            VStack(alignment: .leading) {
+                Text(date)
+                    .font(.headline)
+                    .foregroundColor(.black)
+                
+                Text(description)
+                    .foregroundColor(.gray)
+                
+                Spacer()
+            }
+            Spacer()
+                Text(time)
+                    .font(.headline)
+                    .foregroundColor(.black)
+                
+            }
 
         }
-        .padding(.horizontal)
-        .background(Color.white)
-        .navigationBarHidden(true)
-         // This line looks suspicious, you might want to review it
-
-        // Hide the navigation bar
     }
-}
-
-struct Notifications1_Preview: PreviewProvider {
-    static var previews: some View {
-        Notification1()
+    
+    struct Notifications1_Preview: PreviewProvider {
+        static var previews: some View {
+            Notification1()
+        }
     }
-}
+    
 
