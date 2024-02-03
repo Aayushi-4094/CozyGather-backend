838b4f1d2d57a0e68f3a4a0b52461a7270b08ac1
Aayushi-4094 <aayushiagarwal580@gmail.com>
2024-02-03 20:43:09 +0530

    made changes in the second screen

--- CozyGather/SecondScreen.swift
+++ CozyGather/SecondScreen.swift
@@ -5,33 +5,73 @@ struct SecondScreen: View {
     @State private var goToThirdScreen = false
 
     var body: some View {
-        VStack//(spacing: 3)
-        {
-            Image("logo1")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 1/5)
+        NavigationView {
+            VStack {
+                Image("logo1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: UIScreen.main.bounds.width * 0.6, height: UIScreen.main.bounds.height * 0.15)
+                    .padding(.top, 20)
 
-            //Spacer()
-            
-            Image("logo2")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 1/4)
-                .padding(.bottom, 10)
-        }
-        .onTapGesture {
-            self.goToThirdScreen = true
-        }
-        .fullScreenCover(isPresented: $goToThirdScreen) {
-            ThirdScreen()
+                Text("Where Simplified Planning Meets Stress-Free Celebrations")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, 20)
+
+                Spacer()
+
+                Text("Choose your Profile")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .padding(.top, 20)
+
+                HStack {
+                    VStack {
+                        NavigationLink(destination: VendrSignUp()) {
+                            Image("supplier_logo")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: UIScreen.main.bounds.width * 0.4, height: UIScreen.main.bounds.height * 0.15)
+                                .cornerRadius(20)
+                                .shadow(radius: 5)
+                        }
+
+                        Text("Supplier")
+                            .font(.headline)
+                            .padding(.top, 10)
+                    }
+
+                    Divider()
+
+                    VStack {
+                        NavigationLink(destination: SignUpPage()) {
+                            Image("user_logo")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: UIScreen.main.bounds.width * 0.4, height: UIScreen.main.bounds.height * 0.15)
+                                .cornerRadius(20)
+                                .shadow(radius: 5)
+                        }
+
+                        Text("User")
+                            .font(.headline)
+                            .padding(.top, 5)
+                    }
+                }
+                .padding(.horizontal, 20)
+                .padding(.vertical, 20)
+
+                Spacer()
+            }
+            .navigationBarHidden(true)
+            .navigationBarBackButtonHidden(true)
         }
     }
 }
 
-
-struct SecondScreen_Previews: PreviewProvider{
-    static var previews: some View{
+struct SecondScreen_Previews: PreviewProvider {
+    static var previews: some View {
         SecondScreen()
     }
 }
